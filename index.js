437830e0af0cfa45c8adb02f6cc4f7f4a3975672
Brent Waters <bcwaters@asu.edu>
2019-10-18 22:35:17 -0700

    init commit

---
+++ index.js
@@ -0,0 +1,47 @@
+const https = require('https');
+
+var nonce = 1000000000000
+var largestNonce = 0;
+var nonceBlock = ''
+var largestNonceBlock = ''
+var request = require('request');
+var baseUrl = "https://blockchain.info/rawblock"
+
+
+
+
+var getBlockHash = function (blockHash, depth){
+    
+    request('https://blockchain.info/rawblock/' + blockHash, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+        //console.log(body) // Print the google web page.
+        
+        var newNonce = JSON.parse(body).nonce >>> 0;
+        nonce = newNonce > nonce ? nonce : newNonce;
+        nonceBlock = newNonce > nonce ? nonceBlock : blockHash;
+            largestNonce = newNonce < nonce ? nonce : newNonce;
+        largestNonceBlock  = largestNonce==newNonce ? blockHash : largestNonceBlock;
+        console.log('at depth:' + depth)
+           if(depth>0){
+            getBlockHash(JSON.parse(body).next_block[0], --depth)
+        }else{
+            console.log('smallest nonce: ' + nonce + ' blockHash: '+ nonceBlock)
+                console.log('\nlargest nonce: ' + largestNonce + ' blockHash: '+ largestNonceBlock)
+        }
+        
+     }else{
+         console.log('recovered from unexpected api error')
+         getBlockHash(blockHash, depth)
+     }
+        
+     
+})
+
+
+    
+    
+}
+
+
+getBlockHash('0000000000000000000f0f12f2835fdc3e355ef9a047a174671db02bacbd9bbc', 1000)
+
